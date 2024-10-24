443c1cb0787f7835863115e089658e6cc0e9e7ca
MOHAMMED USMANI <mohammedusmani2005@gmail.com>
2024-10-24 22:52:14 +0530

    accepted viraj's UI

---
+++ education/components/ui/card-hover-effect.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { useState, ReactNode } from "react";
+
+interface Item {
+  link: string;
+  title: string;
+  description: string;
+}
+
+interface HoverEffectProps {
+  items: Item[];
+  className?: string;
+}
+
+interface CardProps {
+  className?: string;
+  children: ReactNode;
+}
+
+interface CardTitleProps {
+  className?: string;
+  children: ReactNode;
+}
+
+interface CardDescriptionProps {
+  className?: string;
+  children: ReactNode;
+}
+
+export const HoverEffect: React.FC<HoverEffectProps> = ({ items, className }) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div className={cn("grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10", className)}>
+      {items.map((item, idx) => (
+        <Link
+          href={item.link}
+          key={item.link}
+          className="relative group block p-2 h-full w-full"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, transition: { duration: 0.15 } }}
+                exit={{ opacity: 0, transition: { duration: 0.15, delay: 0.2 } }}
+              />
+            )}
+          </AnimatePresence>
+          <Card>
+            <CardTitle>{item.title}</CardTitle>
+            <CardDescription>{item.description}</CardDescription>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export const Card: React.FC<CardProps> = ({ className, children }) => {
+  return (
+    <div
+      className={cn(
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        className
+      )}
+    >
+      <div className="relative z-50">
+        <div className="p-4">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export const CardTitle: React.FC<CardTitleProps> = ({ className, children }) => {
+  return (
+    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+      {children}
+    </h4>
+  );
+};
+
+export const CardDescription: React.FC<CardDescriptionProps> = ({ className, children }) => {
+  return (
+    <p className={cn("mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm", className)}>
+      {children}
+    </p>
+  );
+};
--- education/tailwind.config.ts
+++ education/tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -81,11 +81,22 @@ const config = {
   				to: {
   					height: '0'
   				}
-  			}
+  			},
+			'spotlight': {
+				"0%": {
+					opacity: "0",
+					transform: "translate(-72%, -62%) scale(0.5)",
+				},
+				"100%": {
+					opacity: "1",
+					transform: "translate(-50%,-40%) scale(1)",
+				},
+			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+			'spotlight': "spotlight 2s ease .75s 1 forwards",
   		}
   	}
   },
